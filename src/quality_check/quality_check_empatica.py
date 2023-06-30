b3c4b66d905307de883da5b738a8ea11cefdc6b5
Triana Hoyos Ana <trianaa1@cs-054.org.aalto.fi>
2023-06-30 17:53:31 +0300

    FIX: quality checks for the empatica data

--- src/quality_check/quality_check_empatica.py
+++ src/quality_check/quality_check_empatica.py
@@ -20,64 +20,43 @@ import numpy as np
 from glob import glob
 
 path = sys.argv[1]
+start = sys.argv[2]
+end = sys.argv[3]
 
 files = []
-pattern   = f'*device-wristband_*preproc.csv'
+pattern   = f'*device-embraceplus.csv'
 
 for dir,_,_ in os.walk(path):
     files.extend(glob(os.path.join(dir,pattern))) 
 print(f'checking data quality for {str(len(files))} files..................')
 
-variables = []
+cols = ['pulse rate', 'pulse rate variability', 'breathing rate']
+miss_df = pd.DataFrame(columns=cols)
 for file in files:
-    left = 'device-wristband_'
-    right = '_preproc.csv'
-    variables.append(file[file.index(left)+len(left):file.index(right)])
-variables = list(set(variables))
-print(f'checking data quality for {str(len(variables))} variables..................')
-
-final = []
-for variable in variables:
-    print(f'processing {variable}')
-    df=[]
-    for file in files:
-        if variable in file:
-            data = pd.read_csv(file, index_col="time")
-            data["date"] = pd.to_datetime(data.index)
-            data.set_index("date", inplace=True)
+    df = pd.read_csv(file)
     
-            df.append(data)
-
-    data = pd.concat(df)
-    data = data.sort_index()
-
-    if variable=="interbeatinterval":
-        data = data.resample('5min').std()
-        freq = '300S' #5mins
-        num = len(data)
-    elif variable=="hr":
-        freq='S'
-        num=86400
-    else:
-        freq='0.25S' #4Hz sampling
-        num=86400*4 #4times in a second, for all the seconds in a day
-        
-    start_date = data.index[0]
-    end_date = data.index[-1]
-    all_points = pd.date_range(start=start_date, end=end_date, freq=freq)
-    all_points = all_points.to_frame()
-    all_points["data"] = np.nan
-    all_points.rename(columns={0:"date"}, inplace=True)
-
-    df = all_points.set_index('date').join(data, lsuffix="_a")
-    df.drop(columns=["data_a"],inplace=True)
-    df[df["data"]>0] = 0
-    df.fillna(1, inplace=True)
-
-    percentage = df.resample('1D').sum()
-    percentage["data"] = (percentage["data"]/num)*100
-
-    print(f'data loss in percentage for {variable}...............')
-    print(percentage)
+    # Extract the day from the timestamp column
+    day = pd.to_datetime(df['timestamp_iso']).dt.date[0]
     
+    # Calculate the statistics
+    loss_pr = df['pulse_rate_bpm'].isna().sum()
+    loss_prv = df['prv_rmssd_ms'].isna().sum()
+    loss_br = df['respiratory_rate_brpm'].isna().sum()
+
+    # Create a temporary DataFrame to hold the current results
+    temp_df = pd.DataFrame({'day': [day], 'pulse rate': [loss_pr], 
+                            'pulse rate variability': [loss_prv], 'breathing rate': [loss_br]})
     
+    # Concatenate the current results with the results DataFrame
+    miss_df = pd.concat([miss_df, temp_df], ignore_index=True)
+
+miss_df[cols] = miss_df[cols].apply(pd.to_numeric, errors='coerce')
+miss_df['day'] = pd.to_datetime(miss_df['day'])
+miss_df['day'] = miss_df['day'].dt.strftime('%d-%m-%Y')
+miss_df.set_index('day', inplace=True)
+
+#Compute this in percentages
+miss_df = (miss_df/1440)*100
+
+print(f'average data loss in percentage for empatica {miss_df.mean()}')
+
