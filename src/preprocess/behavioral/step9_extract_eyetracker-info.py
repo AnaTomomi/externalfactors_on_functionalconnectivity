6f51ea6c580bb1fe94416e2c1f51d46b943fa4d0
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-07-11 15:55:50 +0300

    update name of the eyetracker preprocessing file

--- src/preprocess/behavioral/step9_extract_eyetracker-info.py
+++ src/preprocess/behavioral/step9_extract_eyetracker-info.py
@@ -1,26 +1,30 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-For full explanations, visit https://risoms.github.io/mdl/docs/build/manual/EyeLink%201000%20Plus%20User%20Manual%201.0.12.pdf
+This script reads the .asc eyetracker files in a folder and extracts information
+about the fixation, saccade, and blinks for each file, following the guidelines in:
+https://risoms.github.io/mdl/docs/build/manual/EyeLink%201000%20Plus%20User%20Manual%201.0.12.pdf
+
+The script returns a dataframe with the information.
+
+Inputs: path 
+
+Outputs: csv file saved in the same input path
 
 @author: trianaa1 based on the parse_eyetrack by @merzonl1
 """
 
-import os, glob
+import os, glob, sys
 import pandas as pd
 import re
-import warnings
-import time as t
 
-df = pd.DataFrame(columns=['SUBJECT', 'TASK', 'EVENT', 'START_TIME', 'END_TIME', 'BLINK_DURATION', 'FIX_DURATION', 'SAC_DURATION', 'FIX_X', 'FIX_Y', 'SAC_START_X', 'SAC_START_Y', 'SAC_END_X', 'SAC_END_Y', 'SAC_AMPLITUDE', 'SAC_PEAK_VELOCITY', 'PUPIL_SIZE'])
+import warnings
+warnings.filterwarnings("ignore")
 
 fix_str = re.compile(r'EFIX(.*)')
 blink_str = re.compile(r'EBLINK(.*)')
 sac_str = re.compile(r'ESACC(.*)')
 message_str = re.compile(r'MSG(.*)')
 
-
-root_dir = '/m/cs/project/networks-pm/mri/fast_prepro_bids/'
+root_dir = sys.argv[1]
 search_str = os.path.join(root_dir, '**', 'func', '*.asc')
 files = glob.glob(search_str, recursive=True)
 
@@ -40,17 +44,19 @@ for file in files:
         tag = 'movie'
     elif task == 'nback':
         tag = 'back'
-        
+     
+    flag = False
+    event = None
+    df = pd.DataFrame(columns=['SUBJECT', 'TASK', 'EVENT', 'START_TIME', 'END_TIME', 'BLINK_DURATION', 'FIX_DURATION', 'SAC_DURATION', 'FIX_X', 'FIX_Y', 'SAC_START_X', 'SAC_START_Y', 'SAC_END_X', 'SAC_END_Y', 'SAC_AMPLITUDE', 'SAC_PEAK_VELOCITY', 'PUPIL_SIZE'])
+
     with open(file) as log:
-        flag = False
-        event = None
         for index, line in enumerate(log):
             line = line.strip()
             
             if message_str.match(line):
                 event = 'message'
                 mes = line.replace('\t', ' ').split()
-                if mes[2] in tag:
+                if tag in mes[2]:
                     flag =True #If the message is detected, it means the stimulus begun, 
                     #so we need to start preprocessing from this point onwards.
                 else:
@@ -93,9 +99,11 @@ for file in files:
                 st_time = time[2]
                 end_time = time[3]
                 dur = time[4]
-                df = df.append({'SUBJECT': sb, 'TASK': task, 'EVENT': event, 'START_TIME': time, 'END_TIME': end_time,
+                df = df.append({'SUBJECT': sb, 'TASK': task, 'EVENT': event, 'START_TIME': st_time, 'END_TIME': end_time,
                                 'BLINK_DURATION': dur}, ignore_index=True)
         
         log.close()
         
-        df.to_csv(f'{root_dir}/{sb}_task-{task}_device-eyetrack.csv')
+        df.to_csv(f'{root_dir}/{sb}/func/{sb}_task-{task}_device-eyetrack.csv')
+        print (f'{root_dir}/{sb}/func/{sb}_task-{task}_device-eyetrack.csv')
+        del df
