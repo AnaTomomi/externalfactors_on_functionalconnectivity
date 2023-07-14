e3783d376e0b7a353a277e46531c5ec739f3085b
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-07-14 16:28:36 +0300

    add more time to the batch

--- src/preprocess/mri/step5_fmriprep.sh
+++ src/preprocess/mri/step5_fmriprep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=2-00:00:00
-#SBATCH --mem=30G
+#SBATCH --time=4-00:00:00
+#SBATCH --mem=40G
 #SBATCH --array=1
-#SBATCH --output=/m/cs/scratch/networks-pm/fmriprep_sub07-%j.out
+#SBATCH --output=/m/cs/scratch/networks-pm/fmriprep_sub30-%j.out
 #SBATCH --cpus-per-task=4
 
 module load singularity-fmriprep/22.1.0
-singularity_wrapper exec fmriprep /m/cs/scratch/networks-pm/pm/  /m/cs/scratch/networks-pm/pm_preprocessed/ -w  /m/cs/scratch/networks-pm/temp/ participant --participant-label sub-07 --fs-no-reconall --output-spaces MNI152NLin6Asym:res-2 --fs-license-file /scratch/shareddata/set1/freesurfer/license.txt
+singularity_wrapper exec fmriprep /m/cs/scratch/networks-pm/pm/  /m/cs/scratch/networks-pm/pm_preprocessed/ -w  /m/cs/scratch/networks-pm/temp/ participant --participant-label sub-30 --fs-no-reconall --output-spaces MNI152NLin6Asym:res-2 --fs-license-file /scratch/shareddata/set1/freesurfer/license.txt
