fd2038d04249b4d8bf2f70c36356d4855968f143
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-10-07 23:00:47 +0300

    ADD: scripts to compute averaged ROI betaseries and the contrast for nback

---
+++ src/analysis/H1/compute_nback_nets.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from scipy.io import savemat, loadmat
+
+###############################################################################
+# Input variables: modify these accordingly
+conn_path = '/m/cs/scratch/networks-pm/effects_externalfactors_on_functionalconnectivity/data/mri/conn_matrix/nback'
+strategy = '24HMP-8Phys-Spike_HPF'
+atlas_name = 'seitzman-set1'
+###############################################################################
+
+oneback = loadmat(f'{conn_path}/{strategy}/reg-adj_oneback_{strategy}_{atlas_name}.mat')['conn'][0]
+oneback = [pd.DataFrame(ts) for ts in oneback]
+
+twoback = loadmat(f'{conn_path}/{strategy}/reg-adj_twoback_{strategy}_{atlas_name}.mat')['conn'][0]
+twoback = [pd.DataFrame(ts) for ts in twoback]
+
+conn=[]
+for ob, tb in zip(oneback, twoback):
+    conn.append(tb - ob)
+
+conn_file = f'{conn_path}/{strategy}/reg-adj_{strategy}_{atlas_name}.mat'
+savemat(conn_file, {'conn':conn})
