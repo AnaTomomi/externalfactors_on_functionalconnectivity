4b8e33608dcedf8355b9d08a711eab523907a6ca
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-10-18 20:48:19 +0300

    add the FDR correction for the links

--- src/analysis/H1/step-6_FDRcorr.m
+++ src/analysis/H1/step-6_FDRcorr.m
@@ -12,8 +12,9 @@ clc
 
 path = '/m/cs/scratch/networks-pm/effects_externalfactors_on_functionalconnectivity/results/H1';
 strategy = '24HMP-8Phys-4GSR-Spike_HPF';
-atlas_name = 'seitzman-set1';
-to_correct = 'parti-coeff'; %'reg-links' or 'parti-coeff'
+atlas_name = 'seitzman-set2';
+atlas_path = '/m/cs/scratch/networks-pm/effects_externalfactors_on_functionalconnectivity/data/mri/conn_matrix/pvt';
+to_correct = 'reg-links'; %'reg-links' or 'parti-coeff'
 
 if strcmp(atlas_name,'seitzman-set1')
     matrixSize = 181;
@@ -33,26 +34,26 @@ totest = unstack(data, 'p_values','Var1'); % Pivot the table
 data_BH = totest;
 data_fdr = totest;
 colNamesToIterate = totest.Properties.VariableNames(2:end);
-for colName = colNamesToIterate
-    pvals = totest.(colName{1});
-    if max(pvals)>1
-        fprintf('%s pval over 1', colName{1})
+if strcmp(to_correct,'parti-coeff')
+    for colName = colNamesToIterate
+        pvals = totest.(colName{1});
+        if max(pvals)>1
+            fprintf('%s pval over 1', colName{1})
+        end
+        qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
+        if all(pvals(:))~=pvals(end)
+            qvals = mafdr(pvals);
+        else
+            qvals = qvals_bh;
+        end
+    
+        if any(qvals_bh<0.05)
+            fprintf('result for %s \n',colName{1})
+        end
+    
+        data_BH.(colName{1}) = qvals_bh;
+        data_fdr.(colName{1}) = qvals;
     end
-    qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
-    if all(pvals(:))~=pvals(end)
-        qvals = mafdr(pvals);
-    else
-        qvals = qvals_bh;
-    end
-
-    if any(qvals_bh<0.05)
-        fprintf('result for %s \n',colName{1})
-    end
-
-    qvals_bh(qvals_bh > 0.05) = 0;
-    qvals(qvals > 0.05) = 0;
-    data_BH.(colName{1}) = qvals_bh;
-    data_fdr.(colName{1}) = qvals;
 end
 
 if strcmp(to_correct,'reg-links')
@@ -61,7 +62,61 @@ if strcmp(to_correct,'reg-links')
     data_BH.('column') = idx(:,2);
     data_fdr.('row') = idx(:,1);
     data_fdr.('column') = idx(:,2);
-end
 
+    colNamesToIterate = totest.Properties.VariableNames(2:end);
+    for colName = colNamesToIterate
+        pvals = totest.(colName{1});
+        if max(pvals)>1
+            fprintf('%s pval over 1', colName{1})
+        end
+        qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
+        if all(pvals(:))~=pvals(end)
+            qvals = mafdr(pvals);
+        else
+            qvals = qvals_bh;
+        end
+    
+        if any(qvals_bh<0.05)
+            fprintf('result for %s \n',colName{1})
+        end
+    
+        data_BH.(colName{1}) = qvals_bh;
+        data_fdr.(colName{1}) = qvals;
+    end
+
+    % keep only the preselected ROIs (DMN, fronto-parietal, cingulo opercular)
+    atlas_info = readtable(sprintf('%s/group_%s_info.xlsx',atlas_path, atlas_name));
+    atlas_info.index = (1:height(atlas_info))';
+    if strcmp(atlas_name,'seitzman-set1')
+        networks = {'DefaultMode', 'FrontoParietal', 'CinguloOpercular','SomatomotorDorsal', 'SomatomotorLateral'};
+        preselection = ismember(atlas_info.netName, networks);
+        atlas_selection = atlas_info(preselection, :);
+    else
+        networks = [1, 3, 9, 10, 11];
+        preselection = ismember(atlas_info.network, networks);
+        atlas_selection = atlas_info(preselection, :);
+    end
+        
+    %select the subnetworks
+    idx = atlas_selection.index;
+    
+    %map to original node numbers in the atlas parcellation 
+    %(and it works!)
+    num_rows_data_BH = height(data_BH);
+    for i = 1:num_rows_data_BH
+        % Get the current row and column values
+        current_row = data_BH.row(i);
+        current_column = data_BH.column(i);
+        
+        % Find the position of the current row and column in the
+        % original atlas
+        original_index_row = idx(current_row);
+        original_index_column = idx(current_column);
+        
+        % Assign the original indexes back to the data_BH table
+        data_BH.row(i) = original_index_row;
+        data_BH.column(i) = original_index_column;
+    end
+end
 writetable(data_BH, sprintf('%s/%s_%s_%s_BHcorrected.xlsx', path, to_correct, strategy, atlas_name))
-writetable(data_fdr, sprintf('%s/%s_%s_%s_FDRcorrected.xlsx', path, to_correct, strategy, atlas_name))
+%writetable(data_fdr, sprintf('%s/%s_%s_%s_FDRcorrected.xlsx', path, to_correct, strategy, atlas_name))
---
+++ src/analysis/H7/step-3_FDRcorr.m
@@ -0,0 +1,81 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This script corrects the obtained results for multiple comparisons using%
+% the Benjamin and Hochberg approach                                      %
+%                                                                         %
+% Author: ana.trianahoyos@aalto.fi                                        %
+% Created: 17.10.2023                                                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all
+close all
+clc
+
+path = '/m/cs/scratch/networks-pm/effects_externalfactors_on_functionalconnectivity/results/H5';
+strategy = '24HMP-8Phys-4GSR-Spike_HPF';
+atlas_name = 'seitzman-set2';
+to_correct = 'parti-coeff'; %'global-eff' or 'parti-coeff'
+lags = 16;
+
+variables = {'total_sleep_duration','awake_time','restless_sleep','pa_mean','pa_std', ...
+             'na_mean','stress_mean','pain_mean','mean_respiratory_rate_brpm', ...
+             'min_respiratory_rate_brpm','max_respiratory_rate_brpm','mean_prv_rmssd_ms',...
+             'min_prv_rmssd_ms','max_prv_rmssd_ms','std_prv_rmssd_ms'};
+
+filepath = sprintf('%s/%s_%s_%s.mat', path, to_correct, strategy, atlas_name);
+data = load(filepath);
+
+%Start the multiple comparison correction
+if strcmp(to_correct, 'global-eff')
+    data_BH = data;
+    data_fdr = data;
+    data = data.pvals;
+    for i=1:size(data,1)
+        pvals = data(i,:);
+        if max(pvals)>1
+            fprintf('%s pval over 1', i)
+        end
+        qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
+        if all(pvals(:))~=pvals(end)
+            qvals = mafdr(pvals);
+        else
+            qvals = qvals_bh;
+        end
+    
+        if any(qvals_bh<0.05)
+            fprintf('result for %s \n',variables{i})
+        end
+    
+        qvals_bh(qvals_bh > 0.05) = 0;
+        qvals(qvals > 0.05) = 0;
+        data_BH(i,:) = qvals_bh;
+        data_fdr(i,:) = qvals;
+    end
+    data_BH = array2table(data_BH');
+    data_BH.Properties.VariableNames = variables;
+    data_fdr = array2table(data_fdr');
+    data_fdr.Properties.VariableNames = variables;
+end 
+
+if strcmp(to_correct,'parti-coeff')
+    num_fields = numel(fieldnames(data));
+
+    %organize the data in a variables, lags, nodes array
+    org_data = zeros(size(variables,2), lags, num_fields);
+    for i = 0:(num_fields-1)
+        fieldname = strcat('node_', string(i));
+        org_data(:, :, i+1) = getfield(data, fieldname);
+    end
+
+    %with lag independence
+    data_BH = zeros(size(org_data));
+    for i=1:length(variables)
+        for j=1:lags
+            pvals = squeeze(org_data(i,j,:));
+            qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
+            qvals_bh(qvals_bh > 0.05) = 0;
+            data_BH(i,j,:) = qvals_bh;
+        end
+    end
+
+end
+
+save(sprintf('%s/%s_%s_%s_BHcorrected.xlsx', path, to_correct, strategy, atlas_name), 'data_BH')
