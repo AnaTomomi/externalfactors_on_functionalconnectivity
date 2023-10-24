6670dcc7f206e7aaa6d405d5847b0206dafadc0c
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-10-24 17:16:25 +0300

    reorganize and rename the files to compute lagged correlations more efficiently for H7

--- src/analysis/H7/step-3_compute_laggedcorr_pc.sh
+++ src/analysis/H7/step-3_compute_laggedcorr_pc.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --time=4-00:00:00
-#SBATCH --mem=100G
+#SBATCH --time=02:30:00
+#SBATCH --mem=20G
 #SBATCH --array=1-4
 #SBATCH --output=/m/cs/scratch/networks-pm/jobs/H7-%j.out
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=4
 
 n=$SLURM_ARRAY_TASK_ID
 variants=`sed "${n}q;d" options.txt`
 
 module load anaconda/2023-01
+export PYTHONUNBUFFERED=true
 python3 /m/cs/scratch/networks-pm/effects_externalfactors_on_functionalconnectivity/src/analysis/H7/compute_lagged_corr_pc.py $variants
