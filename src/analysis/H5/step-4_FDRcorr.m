557a0cf44bb802f4b8881fa31048a439e74c46b5
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-12-21 14:04:39 +0200

    add a for loop to correct for multiple threshold levels at once

--- src/analysis/H5/step-4_FDRcorr.m
+++ src/analysis/H5/step-4_FDRcorr.m
@@ -15,34 +15,40 @@ path = '/m/cs/scratch/networks-pm/effects_externalfactors_on_functionalconnectiv
 strategy = '24HMP-8Phys-Spike_HPF';
 atlas_name = 'seitzman-set1';
 to_correct = 'global-eff'; %'parti-coeff'
-thres = 0.1;
+thresh = [0.1, 0.2, 0.3];
 lags = 16;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 variables = {'total_sleep_duration', 'awake_time','restless_sleep'};
 
-filepath = sprintf('%s/%s_%s_%s_thr-%s.mat', path, to_correct, strategy, atlas_name, num2str(thres*100));
-data = load(filepath);
-
-%Start the multiple comparison correction
-num_fields = numel(fieldnames(data));
-
-%organize the data in a variables, lags, nodes array
-org_data = zeros(size(variables,2), lags, num_fields);
-for i = 0:(num_fields-1)
-    fieldname = strcat('net_', string(i));
-    org_data(:, :, i+1) = getfield(data, fieldname);
-end
-
-%with lag independence
-data_BH = zeros(size(org_data));
-for i=1:length(variables)
-    for j=1:lags
-        pvals = squeeze(org_data(i,j,:));
-        qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
-        data_BH(i,j,:) = qvals_bh;
+for i=1:3
+    thres = thresh(i);
+    filepath = sprintf('%s/%s_%s_%s_thr-%s.mat', path, to_correct, strategy, atlas_name, num2str(thres*100));
+    data = load(filepath);
+    
+    %Start the multiple comparison correction
+    num_fields = numel(fieldnames(data));
+    
+    %organize the data in a variables, lags, nodes array
+    org_data = zeros(size(variables,2), lags, num_fields);
+    for i = 0:(num_fields-1)
+        fieldname = strcat('net_', string(i));
+        org_data(:, :, i+1) = getfield(data, fieldname);
     end
+    
+    %with lag independence
+    data_BH = zeros(size(org_data));
+    for i=1:length(variables)
+        for j=1:lags
+            pvals = squeeze(org_data(i,j,:));
+            qvals_bh =  mafdr(pvals, 'BHFDR', 'True');
+            data_BH(i,j,:) = qvals_bh;
+        end
+    end
+    
+    savefile = sprintf('%s/%s_%s_%s_thr-%s_BHcorrected.mat', path, to_correct, strategy, atlas_name, num2str(thres*100));
+    save(savefile, 'data_BH')
+    clear data_BH
+    clear totest
+    clear org_data
 end
-
-savefile = sprintf('%s/%s_%s_%s_thr-%s_BHcorrected.mat', path, to_correct, strategy, atlas_name, num2str(thres*100));
-save(savefile, 'data_BH')
