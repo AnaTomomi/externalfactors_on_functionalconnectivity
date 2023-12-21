475aed60bb52b58b696061850f341c739a643c09
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-12-21 14:11:37 +0200

    FIX paths for the creatino of fake data

--- src/analysis/create_fake_data.py
+++ src/analysis/create_fake_data.py
@@ -24,6 +24,7 @@ np.random.seed(0)
 
 all_surrogate_behavs = []
 for iteration in range(10000):
+    print(iteration)
     surrogate_behav = pd.DataFrame()
 
     for col in behav.columns:
@@ -46,7 +47,7 @@ for iteration in range(10000):
     
     surrogate_behav['date'] = behav.index
     surrogate_behav.set_index('date', inplace=True)
-    lagged_df = get_behav_data_15days(f'{path}/behavioral', days=16, behav=surrogate_behav)
+    lagged_df = get_behav_data_15days(f'{path}/behavioral/', days=16, behav=surrogate_behav)
     all_surrogate_behavs.append(lagged_df)
 
 with open(f'{path}/behavioral/surrogate_behav_data.pkl', 'wb') as file:
