adfd70689523ca6d933b276241f3aef0da7b71fc
AnaTomomi <ana.trianahoyos@aalto.fi>
2023-12-30 12:42:41 +0200

    adjust for sizes

--- src/visualization/visualize_regressions.py
+++ src/visualization/visualize_regressions.py
@@ -75,7 +75,7 @@ if len(ge)==0 and len(pc)==0 and len(links)>0:
         for col in range(4):
             ax = axes_3d[row * 4 + col]
             npb.plot(template='MNI152NLin2009cAsym',
-                         template_style='glass', template_glass_maxalpha=0.05,
+                         template_style='glass', template_glass_maxalpha=0.2,
                          nodes=nodes, node_type='spheres', node_cmap=cmap, node_color='community',
                          edges=edges,edge_color={'negative':'b','positive':'r'}, edge_weights='weight', edge_widthscale=2,
                          view=views[col], node_colorlegend=False, fontcolor='k', title=None, fig=fig, ax=ax)
@@ -94,44 +94,62 @@ if len(ge)==0 and len(pc)>0 and len(links)>0:
     X = pc["network"].nunique()
     networks = pc["network"].unique()
     
-    fig = plt.figure(figsize=(12, 8))
-    # First grid space with 4 columns and Y rows for plotting the links
-    grid1 = plt.GridSpec(Y, 4, fig, left=0.05, right=0.7, hspace=-0.2, wspace=0.2)        
+    #create the plot first
+    fig = plt.figure(figsize=(15, 12))
+    grid_3d = plt.GridSpec(2, 4, wspace=-0.1, hspace=-0.1, left=0.05, right=0.8)
+    grid_2d = plt.GridSpec(4, 1, wspace=0.1, hspace=0.1, left=0.81, right=0.95)
+    axes_3d, axes_2d = [],[]
+
+    #Plot the links
+    for row in range(2):
+        for col in range(4):
+            ax = fig.add_subplot(grid_3d[row, col], projection='3d')
+            ax.set_title(f'3D Plot {row*4 + col + 1}')
+            axes_3d.append(ax)
+    for row in range(4):
+        if row == 0:
+            ax2 = fig.add_subplot(grid_2d[row:2, 0], projection='3d')
+            ax2.set_title(f'3D Plot in 2D Grid {row + 1}')
+            axes_2d.append(ax2)
+        elif row ==2:
+            ax2 = fig.add_subplot(grid_2d[row, 0])
+            ax2.set_title(f'2D Plot in 2D Grid {row + 1}')
+            axes_2d.append(ax2)
+        else:
+            ax2 = fig.add_subplot(grid_2d[row, 0])
+            ax2.set_visible(False)
+    plt.show()
     
-    for i, factor in enumerate(external_factors):
+    for row, factor in enumerate(external_factors):
         factor_links = links[links["external factor"]==factor]
         nodes, edges = get_nodes_edges(factor_links)
-        for j in range(4):
-            ax = fig.add_subplot(grid1[i, j], projection='3d')
-            npb.plot(template='MNI152NLin2009cAsym', template_style='glass', template_glass_maxalpha=0.01,
+        for col in range(4):
+            ax = axes_3d[row * 4 + col]
+            npb.plot(template='MNI152NLin2009cAsym', template_style='glass', template_glass_maxalpha=0.2,
                      nodes=nodes, node_type='spheres', node_cmap=cmap, node_color='community',
                      edges=edges,edge_color={'negative':'b','positive':'r'}, edge_weights='weight', edge_widthscale=2,
-                     view=views[j], node_colorlegend=False, fontcolor='k', title=None, fig=fig, ax=ax)
-            
-    # Second grid space with 1 column and X*2 rows for plotting the PCs
-    grid2 = plt.GridSpec(X * 2, 1, fig, left=0.8, right=0.95, hspace=-0.2)
-    for i,net in enumerate(networks):
-        #plot the network
-        ax = fig.add_subplot(grid2[i*2, 0], projection='3d')
+                     view=views[col], node_colorlegend=False, fontcolor='k', title=None, fig=fig, ax=ax)
+    #Plot the PC
+    for row,net in enumerate(networks):
+        ax = axes_2d[row]
         nodes_df = get_nodes_from_atlas(atlas_path, atlas_name, net)
-        npb.plot(nodes=nodes_df, template='MNI152NLin2009cAsym', title=None, fig=fig, ax=ax)
+        npb.plot(nodes=nodes_df, node_scale=50, title=None, fig=fig, ax=ax,
+                 template='MNI152NLin2009cAsym', template_style='glass', template_glass_maxalpha=0.2)
         
         #now plot the network vs. external factor
-        ax = fig.add_subplot(grid2[i*2+1, 0])
-        brain2plot = pc["network"].iloc[i]
+        ax = axes_2d[2*row+1]
+        brain2plot = pc["network"].iloc[row]
         y = pd.Series(brain_pc[:,network_mapping[brain2plot]-1], name='pc') #the indices were matlab-based that starts at 1
         x = beh[cols]
         data = pd.concat([y,x], axis=1)
         
-        exog_i = (pc["external factor"].iloc[i]).replace(' ', '_')
+        exog_i = (pc["external factor"].iloc[row]).replace(' ', '_')
         exog_others = list(set(cols) - set([exog_i]))
         sm.graphics.plot_partregress(endog='pc', exog_i=exog_i, exog_others=exog_others, data=data, obs_labels=False, ax=ax)
         ax.set_title('') 
         ax.set_xlabel(f'e({exog_i.replace("_"," ")} | X)')
         ax.set_ylabel('e(participation coefficient | X)')
         
-        ax.set_aspect('equal')
-    
     plt.show()
     plt.savefig(f'{path}/{hypo}_{strategy}_{atlas_name}.pdf', dpi=300)
         
