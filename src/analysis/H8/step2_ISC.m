a51301eb3d997912cd01649a9dc5af575b3996f7
Enrico Glerean <enrico.glerean@aalto.fi>
2024-08-03 17:04:42 +0300

    indentations and comments added

--- src/analysis/H8/step2_ISC.m
+++ src/analysis/H8/step2_ISC.m
@@ -10,36 +10,37 @@ clabels={
  'mov6mm'
 }
 
-for c=1:2
-disp(['subjects' num2str(c) '.m'])
-run(['subjects' num2str(c) '.m'])
+for c=0:3
+	% c is a number that goes from 0 to 3 to follow subjects0.m, subjects1.m, ...
+	disp(['subjects' num2str(c) '.m'])
+	run(['subjects' num2str(c) '.m'])
 
-for s=1:length(subjs)
-	subjects_isc{1,s}=subjs{s};
-end
+	for s=1:length(subjs)
+		subjects_isc{1,s}=subjs{s};
+	end
 
 
 
-outfolder=['/m/triton/scratch/work/eglerean/isctest/ISCout' num2str(c) '/'];
+	outfolder=['/m/triton/scratch/work/eglerean/isctest/ISCout' num2str(c) '/'];
 
-load('demoISCParams.mat')
-Params.PublicParams.removeMemmaps=0;
-Params.PublicParams.dataDescription=clabels{c+1};
-Params.PublicParams.dataDestination=outfolder;
-Params.PublicParams.subjectSource=subjects_isc;
-Params.PublicParams.dataSize=[91 109 91 1059];
-Params.PublicParams.calcStats=1;
-Params.PublicParams.samplingFrequency=1/0.5940;
-Params.PrivateParams.subjectDestination= [outfolder 'fMRIpreprocessed/'];
-Params.PrivateParams.subjectFiltDestination= [outfolder 'fMRIfiltered/'];
-Params.PrivateParams.resultsDestination= [outfolder 'results/'];
-Params.PrivateParams.statsDestination= [outfolder 'stats/'];
-Params.PrivateParams.PFDestination= [outfolder 'PF/'];
-Params.PrivateParams.PFsessionDestination= [outfolder 'PFsession/'];
-Params.PrivateParams.withinDestination= [outfolder 'within/'];
-Params.PrivateParams.phaseDifDestination= [outfolder 'phase/'];
-Params.PrivateParams.dataSize=[91 109 91 1059];
-Params.PrivateParams.nrSubjects=length(subjs);
-runAnalysis(Params)
-clear Params
+	load('demoISCParams.mat')
+	Params.PublicParams.removeMemmaps=0;
+	Params.PublicParams.dataDescription=clabels{c+1}; % labels need to be from 1 to 4, so c is added +1
+	Params.PublicParams.dataDestination=outfolder;
+	Params.PublicParams.subjectSource=subjects_isc;
+	Params.PublicParams.dataSize=[91 109 91 1059];
+	Params.PublicParams.calcStats=1;
+	Params.PublicParams.samplingFrequency=1/0.5940;
+	Params.PrivateParams.subjectDestination= [outfolder 'fMRIpreprocessed/'];
+	Params.PrivateParams.subjectFiltDestination= [outfolder 'fMRIfiltered/'];
+	Params.PrivateParams.resultsDestination= [outfolder 'results/'];
+	Params.PrivateParams.statsDestination= [outfolder 'stats/'];
+	Params.PrivateParams.PFDestination= [outfolder 'PF/'];
+	Params.PrivateParams.PFsessionDestination= [outfolder 'PFsession/'];
+	Params.PrivateParams.withinDestination= [outfolder 'within/'];
+	Params.PrivateParams.phaseDifDestination= [outfolder 'phase/'];
+	Params.PrivateParams.dataSize=[91 109 91 1059];
+	Params.PrivateParams.nrSubjects=length(subjs);
+	runAnalysis(Params)
+	clear Params
 end
